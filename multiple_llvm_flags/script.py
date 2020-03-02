f3219b047c422b0f9da40c9dc79da12b111b0ac6
Luiz Peres <lperesde@sfu.ca>
2020-03-02 15:06:18 -0800

    Adding support for extra flags

--- multiple_llvm_flags/script.py
+++ multiple_llvm_flags/script.py
@@ -22,8 +22,8 @@ def pipe(data, *funcs):
         data = func(data)
     return data
 
-def run(what):
-    return subprocess.check_output(what)
+def run(what, otherflags):
+    return subprocess.check_output(what + otherflags)
 
 def mkname(folder, regex, target, flags, buildfolder):
     buildpath = os.path.join(buildfolder, os.path.join(folder, "bin/icgrep"))
@@ -48,9 +48,9 @@ if __name__ == '__main__':
     argparser.add_argument("-c", "--flags-file", dest="flags", default=os.path.join('.', 'flags'), help="flags filepath")
     argparser.add_argument("-l", "--llvm-file", dest="llvms", default=os.path.join('.', 'llvms'), help="LLVM filepath")
     argparser.add_argument("-b", "--build-path", dest="buildfolder", default=os.path.join('.', 'build'), help="LLVM build folder")
-    argparser.add_argument("-e", "--expression", dest="regex", default="[a-c]", help="Regular expression")
+    argparser.add_argument("-x", "--expression", dest="regex", default="[a-c]", help="Regular expression")
     argparser.add_argument("-t", "--target", dest="target", default=os.path.join('.', 'script.py'), help="File target for comparison")
-    args = argparser.parse_args()
+    args, otherflags = argparser.parse_known_args()
 
     flagset = pipe(args.flags, readFile, allCombinations)
     folders = findLLVMFolders(args.llvms)
@@ -59,7 +59,7 @@ if __name__ == '__main__':
                     flags,
                     breakFlagsIfNeeded,
                     lambda flgs: mkname(folder, args.regex, args.target, flgs, args.buildfolder),
-                    lambda c: run(c)
+                    lambda c: run(c, otherflags)
                 )
         result = pipe(map(mapFn, folders), lambda arr: map(str, arr))
         print(" ".join(result))
